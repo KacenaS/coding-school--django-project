eff0273234b53943c6b88b934173aec9fd3dd55c
KacenaS <simkova1995@outlook.com>
2025-04-09 18:48:05 +0200

    Fix bootstrap5 module name

--- project1/settings.py
+++ project1/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third party apps
-    "bootstrap5",
+    "django_bootstrap5",
     "django_extensions",
     # My apps
     "airlines"
